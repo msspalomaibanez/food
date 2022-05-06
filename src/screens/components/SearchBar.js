4ee4e1f3719beb3aa5ef6d3d60a44e3c7583d869
paloma <palibdev@gmail.com>
2022-05-06 14:37:01 +0200

    added TextInput in Search Bar

--- src/screens/components/SearchBar.js
+++ src/screens/components/SearchBar.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Feather } from '@expo/vector-icons';
+import React from "react";
+import { View, TextInput, StyleSheet } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
 const SearchBar = () => {
-    return (
-        <View style={styles.background}>
-            <Feather name='search' size={30}/>
-            <Text>Search Bar</Text>
-        </View>
-    );
+  return (
+    <View style={styles.background}>
+      <Feather name="search" size={30} />
+      <TextInput placeholder="Search" />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    background: {
-        backgroundColor: '#C4C4C4',
-        height: 50,
-        borderRadius: 5,
-        marginHorizontal: 15
-    }
+  background: {
+    backgroundColor: "#C4C4C4",
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+  },
 });
 
 export default SearchBar;
