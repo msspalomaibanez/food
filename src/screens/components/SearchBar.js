f81b73bd5bf0d2cf64a41b09a6c3c75a56ec55ad
paloma <palibdev@gmail.com>
2022-05-06 13:52:24 +0200

    search bar background color and import into search screen

--- src/screens/components/SearchBar.js
+++ src/screens/components/SearchBar.js
@@ -3,12 +3,16 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const SearchBar = () => {
     return (
-        <View>
+        <View style={styles.background}>
             <Text>Search Bar</Text>
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    background: {
+        backgroundColor: '#C4C4C4'
+    }
+});
 
 export default SearchBar;
